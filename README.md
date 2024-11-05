3bd079c6120af3ca7225d2b5c1c0d54dbc24b283
Craig Hurt <craig.hurt@blakyaks.com>
2024-11-05 16:08:59 +0000

    Updated for 1.1.0 release

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # azure-pipeline-html-reports
-Azure DevOps Extension that enables HTML report output with build pipelines.
+
+An Azure DevOps extension that provides a task for publishing report in a HTML format and embeds it into a Build and Release pages.
+
+## Extension
+
+In order to see report on tab one must first use `Publish HTML Report` task. This is supporting task which makes html tab visible.
+
+This task takes one parameter - required `reportDir` which is a path to report directory. The optional `tabName` parameter may also be supplied to configure the name of the tab displayed within the Azure DevOps UI.
+
+### Example YAML setup
+
+```YAML
+steps:
+  - task: PublishHtmlReport@1
+    displayName: 'Publish HTML Report'
+    inputs:
+      reportDir: '$(ResultsPath)/reportName.html'
+```
+
+### 2023-05-10 - Fixed tab name
+
+This extension patches the original [HTML Viewer by Jakub Rumpca](https://marketplace.visualstudio.com/items?itemName=JakubRumpca.azure-pipelines-html-report) and resolves [#8 TabName incorrectly renders when using multi-stage pipelines](https://github.com/JakubRumpca/azure-pipeline-html-report/issues/8)
+
+Before fix:
+
+![githubIssue8.png](https://github.com/FreakinWard/azure-pipeline-html-report/blob/main/docs/githubIssue8.png?raw=true)
+
+After fix:
+
+![githubIssue8-fixed.png](https://github.com/FreakinWard/azure-pipeline-html-report/blob/main/docs/githubIssue8-fixed.png?raw=true)
+
+```yaml
+
+# tabName has a known bug w/multi-stages: https://github.com/JakubRumpca/azure-pipeline-html-report/issues/8
+- task: PublishHtmlReport@1
+  displayName: Publish E2E Test Report
+  condition: succeededOrFailed()
+  inputs:
+    reportDir: 'cypress/reports/index.html'
+    tabName: 'E2E ${{ parameters.region }}-${{ parameters.slotName }}'
+```
+
+### 2024-11-01 - Version 1.1.0 - Patch Release
+
+This version patches the [AwardedSolutions](https://github.com/FreakinWard/azure-pipeline-html-report) release and is now maintained for future support purposes. The BlakYaks release supports Node 16 and Node 20 runners, removing the deprecation notice displayed during pipeline runs.
---
+++ src/tabContent.tsx
@@ -0,0 +1,175 @@
+import "./tabContent.scss"
+
+import * as React from "react"
+import * as ReactDOM from "react-dom"
+import * as SDK from "azure-devops-extension-sdk"
+
+import { getClient } from "azure-devops-extension-api"
+import { Build, BuildRestClient, Attachment } from "azure-devops-extension-api/Build"
+
+import { ObservableValue, ObservableObject } from "azure-devops-ui/Core/Observable"
+import { Observer } from "azure-devops-ui/Observer"
+import { Tab, TabBar, TabSize } from "azure-devops-ui/Tabs"
+
+
+const ATTACHMENT_TYPE = "report-html";
+
+SDK.init()
+SDK.ready().then(() => {
+  try {
+    const config = SDK.getConfiguration()
+    config.onBuildChanged((build: Build) => {
+      let buildAttachmentClient = new BuildAttachmentClient(build)
+      buildAttachmentClient.init().then(() => {
+        displayReports(buildAttachmentClient)
+      }).catch(error => {throw new Error(error)})
+    })
+  } catch(error) {
+    throw new Error(error)
+  }
+})
+
+function displayReports(attachmentClient: AttachmentClient) {
+  ReactDOM.render(<TaskAttachmentPanel attachmentClient={attachmentClient} />, document.getElementById("html-report-extention-container"))
+}
+
+abstract class AttachmentClient {
+  protected attachments: Attachment[] = []
+  protected authHeaders: Object = undefined
+  protected reportHtmlContent: string = undefined
+  constructor() {}
+
+  public getAttachments() : Attachment[] {
+    return this.attachments
+  }
+
+  public getDownloadableAttachment(attachmentName: string): Attachment {
+    const attachment = this.attachments.find((attachment) => { return attachment.name === attachmentName})
+    if (!(attachment && attachment._links && attachment._links.self && attachment._links.self.href)) {
+      throw new Error("Attachment " + attachmentName + " is not downloadable")
+    }
+    return attachment
+  }
+
+  public async getAttachmentContent(attachmentName: string): Promise<string> {
+    if (this.authHeaders === undefined) {
+      console.log('Get access token')
+      const accessToken = await SDK.getAccessToken()
+      const b64encodedAuth = btoa(':' + accessToken);
+      this.authHeaders = { headers: {'Authorization': 'Basic ' + b64encodedAuth} }
+    }
+    console.log("Get " + attachmentName + " attachment content")
+    const attachment = this.getDownloadableAttachment(attachmentName)
+    const response = await fetch(attachment._links.self.href, this.authHeaders)
+    if (!response.ok) {
+      throw new Error(response.statusText)
+    }
+    const responseText = await response.text()
+    console.log(responseText)
+    return responseText
+  }
+
+}
+
+class BuildAttachmentClient extends AttachmentClient {
+  private build: Build
+
+  constructor(build: Build) {
+    super()
+    this.build = build
+  }
+
+  public async init() {
+    const buildClient: BuildRestClient = getClient(BuildRestClient)
+    this.attachments = await buildClient.getAttachments(this.build.project.id, this.build.id, ATTACHMENT_TYPE)
+    console.log(this.attachments)
+  }
+}
+
+
+interface TaskAttachmentPanelProps {
+  attachmentClient: AttachmentClient
+}
+
+export default class TaskAttachmentPanel extends React.Component<TaskAttachmentPanelProps> {
+  private selectedTabId: ObservableValue<string>
+  private tabContents: ObservableObject<string>
+  private tabInitialContent: string = '<div class="wide"><p>Loading...</p></div>'
+
+  constructor(props: TaskAttachmentPanelProps) {
+    super(props);
+    this.selectedTabId = new ObservableValue(props.attachmentClient.getAttachments()[0].name)
+    this.tabContents = new ObservableObject()
+  }
+
+  public escapeHTML(str: string) {
+    return str.replace(/[&<>'"]/g, tag => ({
+      '&': '&amp;',
+      '<': '&lt;',
+      '>': '&gt;',
+      "'": '&#39;',
+      '"': '&quot;'
+    }[tag] || tag))
+  }
+
+  public render() {
+    const attachments = this.props.attachmentClient.getAttachments()
+    if (attachments.length == 0) {
+      return (null)
+    } else {
+      const tabs = []
+      for (const attachment of attachments) {
+        const metadata = attachment.name.split('.')
+
+        // Determine the tab name and optionally add a badge count
+        let name = metadata[0];
+        let badgeCount = undefined; // Default badgeCount is undefined which means no badge is shown
+
+        if (metadata[2] !== '__default') {
+            badgeCount = parseInt(metadata[3]); // Convert metadata[3] to an integer for badgeCount
+        }
+
+        tabs.push(<Tab
+          name={name}
+          id={attachment.name}
+          key={attachment.name}
+          url={attachment._links.self.href}
+          badgeCount={badgeCount} // This will only be added if badgeCount is not undefined
+        />)
+
+        this.tabContents.add(attachment.name, this.tabInitialContent)
+      }
+
+      return (
+        <div className="flex-column">
+          { attachments.length > 0 ?
+            <TabBar
+              onSelectedTabChanged={this.onSelectedTabChanged}
+              selectedTabId={this.selectedTabId}
+              tabSize={TabSize.Tall}
+              >
+              {tabs}
+            </TabBar>
+          : null }
+          <Observer selectedTabId={this.selectedTabId} tabContents={this.tabContents}>
+            {(props: { selectedTabId: string }) => {
+              if ( this.tabContents.get(props.selectedTabId) === this.tabInitialContent) {
+                this.props.attachmentClient.getAttachmentContent(props.selectedTabId).then((content) => {
+                  this.tabContents.set(props.selectedTabId, '<iframe class="wide flex-row flex-center" srcdoc="' + this.escapeHTML(content) + '"></iframe>')
+              })
+            }
+              return <span dangerouslySetInnerHTML={ {__html: this.tabContents.get(props.selectedTabId)} } />
+            }}
+          </Observer>
+        </div>
+      );
+    }
+  }
+
+  private onSelectedTabChanged = (newTabId: string) => {
+    this.selectedTabId.value = newTabId;
+  }
+}
+
+
+
