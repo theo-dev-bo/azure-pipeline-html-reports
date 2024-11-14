a19f1fe0a54e574ab70c534db145606523bea2c4
Craig Hurt <craig.hurt@blakyaks.com>
2024-11-14 19:44:59 +0000

    Updates for v1.2.0

--- PublishHtmlReport/index.js
+++ PublishHtmlReport/index.js
@@ -1,60 +1,74 @@
-const tl = require('azure-pipelines-task-lib');
-const { resolve, basename, join } = require('path');
-const dashify = require('dashify')
-const globby = require('globby')
-const { readFileSync, writeFileSync } = require('fs')
-const { load } = require('cheerio')
+const tl = require('azure-pipelines-task-lib/task');
+const globby = require('globby');
+const { readFileSync, writeFileSync, statSync } = require('fs');
+const { resolve, basename, dirname } = require('path');
+const cheerio = require('cheerio');
+const dashify = require('dashify');
 
-function run () {
-    let reportDir = tl.getPathInput('reportDir', true, true);
-    let files = globby.sync([reportDir.replace(/\\/g, '/')], {expandDirectories : {files: ['*'], extensions: ['html']}})
-
-    const fileProperties = []
-
-    files.forEach(file => {
-      tl.debug(`Reading report ${file}`)
-      const fileContent = readFileSync(file).toString()
-      const document = load(fileContent)
-      writeFileSync(file, document.html())
-
-      const attachmentProperties = {
-        name: generateName(basename(file)),
-        type: 'report-html'
-      }
-
-      fileProperties.push(attachmentProperties)
-      tl.command('task.addattachment', attachmentProperties, file)
-
-    })
-
-    const jobName = dashify(tl.getVariable('Agent.JobName'))
-    const stageName = dashify(tl.getVariable('System.StageDisplayName'))
-    const stageAttempt = tl.getVariable('System.StageAttempt')
-    const tabName = tl.getInput('tabName', false ) || 'Html-Report'
-    const summaryPath = resolve(reportDir)
-    writeFileSync(summaryPath, JSON.stringify(fileProperties))
-    console.log(summaryPath)
-    tl.addAttachment('report-html', `${tabName}.${jobName}.${stageName}.${stageAttempt}`, summaryPath)
+function addTaskAttachment(attachmentProps) {
+    tl.addAttachment(attachmentProps.type, attachmentProps.name, attachmentProps.path);
 }
 
-function generateName (fileName) {
-    const jobName = dashify(tl.getVariable('Agent.JobName'))
-    const stageName = dashify(tl.getVariable('System.StageDisplayName'))
-    const stageAttempt = tl.getVariable('System.StageAttempt')
-    const tabName = tl.getInput('tabName', false ) || 'Html-Report'
+function run() {
+    try {
+        const reportInputPath = tl.getPathInput('reportDir', true, false);
+        let files = [];
+        let summaryPath;
 
-    return `${tabName}.${jobName}.${stageName}.${stageAttempt}.${fileName}`
+        // Check if reportInputPath is a directory or a file
+        if (statSync(reportInputPath).isDirectory()) {
+            files = globby.sync([`${reportInputPath}/**/*.html`]); // Find all HTML files within the directory
+            summaryPath = resolve(reportInputPath, 'summary.json'); // Place summary in the directory
+        } else {
+            files = [reportInputPath]; // Single file case
+            summaryPath = resolve(dirname(reportInputPath), 'summary.json'); // Place summary in the directory containing the file
+        }
+
+        const fileProperties = [];
+        const summaryProperties = {
+            name: generateSummaryName(),
+            type: 'report-html-summary',
+            path: summaryPath
+        };
+
+        files.forEach(file => {
+            tl.debug(`Reading report ${file}`);
+            const fileContent = readFileSync(file, 'utf8');
+            const document = cheerio.load(fileContent);
+            writeFileSync(file, document.html());
+
+            const attachmentProperties = {
+                name: generateAttachmentName(basename(file)),
+                type: 'report-html',
+                path: file
+            };
+
+            fileProperties.push(attachmentProperties);
+            addTaskAttachment(attachmentProperties);
+        });
+
+        // Save the summary file
+        writeFileSync(summaryPath, JSON.stringify(fileProperties));
+        addTaskAttachment(summaryProperties);
+    } catch (error) {
+        tl.setResult(tl.TaskResult.Failed, error.message);
+    }
 }
 
-try {
-    let reportDir = tl.getPathInput('reportDir', true, true);
-    const jobName = dashify(tl.getVariable('Agent.JobName'))
-    const stageName = dashify(tl.getVariable('System.StageDisplayName'))
-    const stageAttempt = tl.getVariable('System.StageAttempt')
-    const tabName = tl.getInput('tabName', false ) || 'Html-Report'
-    let path = resolve(reportDir)
-    console.log(path)
-    tl.addAttachment('report-html', `${tabName}.${jobName}.${stageName}.${stageAttempt}`, path)
-} catch (error) {
-    tl.setResult(tl.TaskResult.SucceededWithIssues, error.message);
+function generateAttachmentName(fileName) {
+    const jobName = dashify(tl.getVariable('Agent.JobName'));
+    const stageName = dashify(tl.getVariable('System.StageDisplayName'));
+    const stageAttempt = tl.getVariable('System.StageAttempt');
+    const tabName = tl.getInput('tabName', false) || 'Html-Report';
+    return `${tabName}.${jobName}.${stageName}.${stageAttempt}.${fileName}`;
 }
+
+function generateSummaryName() {
+    const jobName = dashify(tl.getVariable('Agent.JobName'));
+    const stageName = dashify(tl.getVariable('System.StageDisplayName'));
+    const stageAttempt = tl.getVariable('System.StageAttempt');
+    const tabName = tl.getInput('tabName', false) || 'Html-Report';
+    return `${tabName}.${jobName}.${stageName}.${stageAttempt}`;
+}
+
+run();
--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Html Reports (BlakYaks.azure-pipeline-html-reports)
 
-An Azure DevOps extension that provides a task for publishing report in a HTML format and embeds it into a Build and Release pages.
+An Azure DevOps extension that provides a task for publishing HTML formatted reports onto build and release pages.
 
 ## Using the extension
 
 In order to see a report tab you must use the  `Publish HTML Report` task. This is a supporting task which adds HTML output from the build pipeline(s) for viewing.
 
-The task takes one mandatory parameter `reportDir` which should reference the HTML filename to be published. The optional `tabName` parameter may also be supplied to configure the name of the tab displayed under `Reports` in the Azure DevOps UI.
+The task takes one mandatory parameter `reportDir` which should reference the HTML filename or directory to be published. The optional `tabName` parameter may also be supplied to configure the name of the tab displayed under `Reports` in the Azure DevOps UI.
 
 ### Example YAML setup
 
@@ -21,27 +21,26 @@ steps:
 
 ## Changelog
 
-### v1.1.1 - BlakYaks Release
+### v1.2.0
+
+- Addresses this [issue](https://github.com/blakyaks/azure-pipeline-html-reports/issues/2) which was carried over from the original source project. The `reportDir` property now supports both directory and file paths; if a directory is specified, all `*.html` files in that directory will be displayed. An example is shown below:
+
+```yaml
+- task: PublishHtmlReport@1
+  displayName: Publish Directory Reports
+  condition: succeededOrFailed()
+  inputs:
+    reportDir: 'cypress/reports'
+    tabName: 'E2E ${{ parameters.region }}-${{ parameters.slotName }}'
+```
+
+### v1.1.1
 
 This version patches the [AwardedSolutions](https://github.com/FreakinWard/azure-pipeline-html-report) release and is now maintained for future support purposes. The BlakYaks release supports Node 16 and Node 20 runners, removing the deprecation notice displayed during pipeline runs.
 
 Task names and inputs were maintained to simplify update from previous releases.
 
-### v1.0.8
-
-This extension patches the original [HTML Viewer by Jakub Rumpca](https://marketplace.visualstudio.com/items?itemName=JakubRumpca.azure-pipelines-html-report) and resolves [#8 TabName incorrectly renders when using multi-stage pipelines](https://github.com/JakubRumpca/azure-pipeline-html-report/issues/8)
-
-Before fix:
-
-![githubIssue8.png](https://github.com/FreakinWard/azure-pipeline-html-report/blob/main/docs/githubIssue8.png?raw=true)
-
-After fix:
-
-![githubIssue8-fixed.png](https://github.com/FreakinWard/azure-pipeline-html-report/blob/main/docs/githubIssue8-fixed.png?raw=true)
-
 ```yaml
-
-# tabName has a known bug w/multi-stages: https://github.com/JakubRumpca/azure-pipeline-html-report/issues/8
 - task: PublishHtmlReport@1
   displayName: Publish E2E Test Report
   condition: succeededOrFailed()
@@ -49,4 +48,3 @@ After fix:
     reportDir: 'cypress/reports/index.html'
     tabName: 'E2E ${{ parameters.region }}-${{ parameters.slotName }}'
 ```
-
